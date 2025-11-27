cc1394c5401c918a55d7b41b083df19fbdac2fc0
Abhinivesh2729 <abhinivesh.smail@gmail.com>
2025-11-27 14:00:58 +0530

    git ignore added

--- agent/agent.py
+++ agent/agent.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from pathlib import Path
-
+from langchain_google_genai import ChatGoogleGenerativeAI
 #add project root to Python path
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
@@ -10,6 +10,24 @@ from utils.env_helper import load_env_vars
 
 load_env_vars()
 api_key = os.getenv("GEMINI_API_KEY")
-print(api_key)
+#print(api_key)
 
+
+
+
+#model configs
+
+basic_model = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-flash-lite",
+    api_key = api_key
+)
+
+advance_model = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-flash",
+    api_key = api_key
+)
+
+#print(advance_model.invoke("Hello, who are you").content)
+
+#tools
 
--- utils/env_helper.py
+++ utils/env_helper.py
@@ -3,10 +3,6 @@ from dotenv import load_dotenv
 
 def load_env_vars(env_path=".env"):
     load_dotenv(env_path, override=True)
-    # Optionally, print loaded variables
-    for key, value in os.environ.items():
-        if key.startswith("GEMINI_"):
-            print(f"{key}={value}")
 
 # Usage
 load_env_vars()
